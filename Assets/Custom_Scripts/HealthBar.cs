6167d8bbaf62db457af1f72d1e5ec80e1abbd9a7
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-03-23 21:00:14 -0400

    copied over Proto 3

---
+++ Assets/Custom_Scripts/HealthBar.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image currentHealth;
+    public Text ratio;
+
+    private float hitpoint = 100f;
+    private float maxHitpoint = 100f;
+
+
+    private void Start()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        float r = hitpoint / maxHitpoint;
+        currentHealth.rectTransform.localScale = new Vector3(r, 1, 1);
+        ratio.text = (r * 100).ToString("0") + '%';
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hitpoint -= damage;
+        if (hitpoint < 0)
+        {
+            hitpoint = 0;
+            Debug.Log("Dead!");
+            SceneManager.LoadScene("GameOver_Lose", LoadSceneMode.Single);
+        }
+
+        UpdateHealthBar();
+    }
+
+    public void HealthDamage(float heal)
+    {
+        hitpoint += heal;
+        if (hitpoint > maxHitpoint)
+        {
+            hitpoint = maxHitpoint;
+        }
+
+        UpdateHealthBar();
+    }
+}
---
+++ Assets/Custom_Scripts/Vegetable.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class Vegetable : MonoBehaviour
+{
+    private bool f_pressed;
+    private bool e_pressed;
+
+    public float speed = 1000f; 
+    //public GameObject vege; 
+    public Transform p; //player 
+
+    private NavMeshAgent temp;
+
+    public float time = 1000f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        f_pressed = Input.GetKeyDown("f");
+       
+        /*
+        e_pressed = Input.GetKeyDown("e"); 
+
+        if (e_pressed)
+        {
+            Debug.Log("Pressed e");
+            this.gameObject.SetActive(true);
+            //transform.position = Vector3.MoveTowards(transform.position, p.position, speed * Time.deltaTime); 
+            //Instantiate(vege, p.position, Quaternion.identity); 
+        }
+        */
+
+    }
+
+   
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Found player by vegetable"); 
+            if(f_pressed)
+            {
+                this.gameObject.SetActive(false); 
+            }
+        }
+
+        if(other.gameObject.CompareTag("AI"))
+        {
+            Debug.Log("AI ennemy detected");
+            temp = other.gameObject.GetComponent<NavMeshAgent>();
+
+            //temp.Stop(); 
+            //temp.isStopped = true;
+
+            //StartCoroutine(countDown(temp));
+
+            /*
+            while (time > 0)
+            {
+                time -= Time.deltaTime;
+                Debug.Log(time);
+            }
+            */
+
+            /*
+            while(time > 0)
+            {
+                time -= 1f;
+                Debug.Log(time);
+            }
+            */
+
+            //Debug.Log("back into here");
+
+            //temp.isStopped = false; 
+
+            //temp.Resume(); 
+
+            //temp.angularSpeed = 0;
+           
+           temp.speed = 0.7f;
+
+           this.gameObject.SetActive(false); 
+        }
+    }
+
+
+    /*
+    IEnumerator countDown(NavMeshAgent temp)
+    {
+        Debug.Log("Inside countdown"); 
+
+   
+        yield return new WaitForSeconds(time);
+        
+        while(time > 0)
+        {
+            time -= Time.deltaTime;
+            Debug.Log(time); 
+        }
+
+    }
+    */
+
+
+}
