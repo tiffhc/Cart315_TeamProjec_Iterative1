c187d2783aaa6569ced14c02911b9a65de7b3304
Liu WenYue <LiuWY0912@gmail.com>
2020-04-05 03:44:09 -0400

    IP1: Testing with red health bar

--- Assets/Custom_Scripts/HealthBar.cs
+++ Assets/Custom_Scripts/HealthBar.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 public class HealthBar : MonoBehaviour
 {
     public Image currentHealth;
+    public Image LowHealth;
     public Text ratio;
+    //public GameObject red;
+
 
     private float hitpoint = 100f;
     private float maxHitpoint = 100f;
@@ -21,11 +24,17 @@ public class HealthBar : MonoBehaviour
     {
         float r = hitpoint / maxHitpoint;
         currentHealth.rectTransform.localScale = new Vector3(r, 1, 1);
+        LowHealth.rectTransform.localScale = new Vector3(r, 1, 1);
         ratio.text = (r * 100).ToString("0") + '%';
     }
 
     public void TakeDamage(float damage)
     {
+        if (hitpoint < 20)
+        {
+            LowHealth.SetActive(true);
+        }
+
         hitpoint -= damage;
         if (hitpoint < 0)
         {
@@ -35,6 +44,7 @@ public class HealthBar : MonoBehaviour
         }
 
         UpdateHealthBar();
+
     }
 
     public void HealthDamage(float heal)
